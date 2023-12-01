f1b63a5db45032c71f8eb0beec4df008e09c3ba5
metaironia <serezha364@gmail.com>
2023-12-01 04:00:11 +0300

    separated math commands from differentiator func

--- differentiator_func.h
+++ differentiator_func.h
@@ -10,22 +10,6 @@
                                     }                                    \
                                 }
 
-#define dL                      (FindNodeDerivative (math_expression_tree_node -> left_branch))
-
-#define dR                      (FindNodeDerivative (math_expression_tree_node -> right_branch))
-
-#define ADD_(...)               (CreateNode (BINARY_OPERATOR, OPERATOR_ADD, __VA_ARGS__))
-
-#define SUB_(...)               (CreateNode (BINARY_OPERATOR, OPERATOR_SUB, __VA_ARGS__))
-
-#define MUL_(...)               (CreateNode (BINARY_OPERATOR, OPERATOR_MUL, __VA_ARGS__))
-
-#define DIV_(...)               (CreateNode (BINARY_OPERATOR, OPERATOR_DIV, __VA_ARGS__))
-
-#define POW_(...)               (CreateNode (BINARY_OPERATOR, OPERATOR_POW, __VA_ARGS__))
-
-#define NUM_(...)               (CreateNode (NUMBER, __VA_ARGS__, NULL, NULL))
-
 enum MathNodeOperator {
 
     OPERATOR_ADD,
